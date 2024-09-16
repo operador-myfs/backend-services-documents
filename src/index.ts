ff1b975a539e80af1953ea22476cdc90c7db9ff2
Alejandro <realpealejo@gmail.com>
2024-09-15 23:43:40 -0500

    feat: Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import cors from 'cors';
 import express, { Application } from 'express';
 import { createServer } from 'http';
 import router from './router';
@@ -8,12 +9,26 @@ import helmet from 'helmet';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 
+const whitelist = ['/'];
+const options: cors.CorsOptions = {
+  origin: whitelist,
+};
 const port = process.env.PORT || 8000;
+const env = process.env.NODE_ENV || 'production';
 
 const app: Application = express();
 
 app.use(express.json({ limit: '20kb' }));
 app.use(helmet());
+
+if (env === 'production') {
+  app.use(cors(options));
+}
+
+if (env === 'development') {
+  app.use(cors());
+}
+
 app.disable('x-powered-by');
 
 app.use('/document/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
