2cfd9aa08561ba35bb22ff22b9dec97265f8d6bd
Alejandro <realpealejo@gmail.com>
2024-10-02 23:48:44 -0500

    feat: Adjust auth middleware

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -1,42 +1,53 @@
-// import response from '../utils/response';
-// import { auth } from '../firebase';
 import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
+import response from '../utils/response';
+
+const client = jwksClient({
+  jwksUri: `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${process.env.COGNITO_POOL_ID}/.well-known/jwks.json`,
+});
+
+function getKey(header: any, callback: any) {
+  client.getSigningKey(header.kid, (err, key) => {
+    const signingKey = key?.getPublicKey();
+    callback(null, signingKey);
+  });
+}
 
 const authorization = async (req: Request, res: Response, next: NextFunction) => {
-  req.body.uid = '12345';
-  next();
-  return;
-  // TODO validate token
-  // if (!req.headers.authorization || req.headers.authorization.indexOf('Bearer ') === -1) {
-  //   return response({
-  //     res,
-  //     status: 401,
-  //     error: true,
-  //     message: 'No está autorizado para realizar esa operación',
-  //   });
-  // }
-  // try {
-  //   var t = req.headers.authorization.replace('Bearer ', '');
-  //   const JWT = await auth.verifyIdToken(t);
-  //   if (JWT) {
-  //     req.body.uid = JWT.uid;
-  //     next();
-  //   } else {
-  //     return response({
-  //       res,
-  //       status: 401,
-  //       error: true,
-  //       message: 'No está autorizado para realizar esa operación',
-  //     });
-  //   }
-  // } catch (error) {
-  //   return response({
-  //     res,
-  //     status: 500,
-  //     error: true,
-  //     message: 'Error al autenticar petición',
-  //   });
-  // }
+  if (!req.headers.authorization || req.headers.authorization.indexOf('Bearer ') === -1) {
+    return response({
+      res,
+      status: 401,
+      error: true,
+      message: 'No está autorizado para realizar esa operación',
+    });
+  }
+
+  const token = req.headers.authorization.replace('Bearer ', '');
+  console.log(token);
+  jwt.verify(
+    token,
+    getKey,
+    {
+      algorithms: ['RS256'],
+    },
+    (err, decoded) => {
+      if (err) {
+        console.log('error', err);
+        return response({
+          res,
+          status: 401,
+          error: true,
+          message: 'No está autorizado para realizar esa operación',
+        });
+      }
+
+      const email = (decoded as any).email.toLowerCase();
+      req.body.uid = email;
+      next();
+    }
+  );
 };
 
 export default authorization;
